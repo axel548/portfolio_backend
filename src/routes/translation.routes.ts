3faf95aa519ef065293e826bfce4cdcef077090d
Axel Lopez <axel.aatl@gmail.com>
2025-10-10 23:39:14 -0600

    [DELETE] API Languages

--- src/routes/translation.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getTranslations } from '../controllers/translation.controller';
-
-const router = Router();
-
-router.get('/:lang', getTranslations);
-
-export default router;
--- src/types/index.ts
+++
@@ -1,5 +0,0 @@
-
-export interface Translation {
-  title: string;
-  subtitle: string;
-}
