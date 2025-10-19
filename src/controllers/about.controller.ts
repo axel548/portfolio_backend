84e28a91ccacf07ce65d6e0400d3bfae85c6fb51
Axel Lopez <axel.aatl@gmail.com>
2025-10-19 00:50:41 -0600

    [ADD] Endpoint Put in About texts

--- src/controllers/about.controller.ts
+++ src/controllers/about.controller.ts
@@ -14,3 +14,28 @@ export const getAbout = async (req: Request, res: Response) => {
     res.status(500).send("Error fetching about data");
   }
 };
+
+export const updateAbout = async (req: Request, res: Response) => {
+    const { lang, title, description, button } = req.body;
+
+    try {
+        let about = await About.findOne({ lang });
+
+        if (!about) {
+            return res.status(404).json({ msg: 'About data not found for the specified language' });
+        }
+
+        about.title = title || about.title;
+        about.description = description || about.description;
+        about.button = button || about.button;
+
+        await about.save();
+
+        res.json(about);
+    } catch (err) {
+        if (err instanceof Error) {
+            console.error(err.message);
+        }
+        res.status(500).send('Server Error');
+    }
+};
--- src/routes/about.routes.ts
+++ src/routes/about.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getAbout } from "../controllers/about.controller";
+import { getAbout, updateAbout } from "../controllers/about.controller";
 
 const router = Router();
 
 router.get("/", getAbout);
+router.put("/", updateAbout);
 
 export default router;
